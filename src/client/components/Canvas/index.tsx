56d5dbb8a86d7c9a36faf3d0c006431e5122e1c0
olafkotur <olafkotur97@gmail.com>
2020-04-01 16:48:38 +0100

    refactored how tiles are handlded

--- src/client/components/Canvas/index.tsx
+++ src/client/components/Canvas/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles';
 import { View, Dimensions } from 'react-native';
 import Tile from '../Tile';
 import { PatternService } from '../../../services/pattern';
-import { game } from '../../../config';
+import { game, theme } from '../../../config';
 
 interface IProps {
   pattern: number[][];
@@ -22,68 +22,41 @@ export default class Canvas extends React.Component<IProps> {
   width: any = { width: this.props.size ? (this.props.size * game.tileSize) + (this.props.size * 5 - 1): '100%' }
   counter: number = 0;
 
-  handleTilePressed = (counter: number, symbol: number) => {
-    this.tiles[counter] = this.detectTile(symbol, counter);
+  handleTilePressed = (col: number, row: number, symbol: number) => {
+    this.tiles.forEach((tile: JSX.Element, i: number) => {
+      const newSymbol: number = symbol === 1 ? 0 : 1;
+      if (tile.key === `${col}:${row}`) {
+        this.tiles[i] =
+        <Tile
+          key={ `${col}:${row}` }
+          col={ col } row={ row } symbol={ newSymbol }
+          color={ newSymbol === 1 ? this.props.color : theme.white }
+          blank={ row === -1 ? true : false }
+          handleTilePressed={ this.handleTilePressed }
+        />
+      }
+    });
+    this.setState({});
   };
 
   generateTiles = (): void => {
     const pattern: number[][] = this.props.pattern;
-    this.tiles = [];
 
     // Generate tiles based on pattern
-    let counter: number = 0;
+    this.tiles = [];
     pattern.forEach((col: number[], i: number) => {
       col.forEach((row: number, j: number) => {
-        this.tiles.push(this.detectTile(row, counter));
-        counter++;
+        this.tiles.push(
+          <Tile
+            key={ `${i}:${j}` }
+            col={ i } row={ j } symbol={ row }
+            color={ row === 1 ? this.props.color : theme.white }
+            blank={ row === -1 ? true : false }
+            handleTilePressed={ this.handleTilePressed }
+          />
+        );
       });
     });
-  };
-
-  detectTile = (symbol: number, counter: number): JSX.Element => {
-    if (symbol === 1) {
-      return ( 
-        <Tile
-          key={ Math.random() } 
-          color={ this.props.color } 
-          active
-          margin={ this.props.margin }
-          handleTilePressed={ (): void => this.handleTilePressed(counter, symbol) }
-        />
-      );
-    }
-    else if (symbol === 0) {
-      return ( 
-        <Tile 
-          key={ Math.random() } 
-          color={ this.props.color } 
-          margin={ this.props.margin }
-          handleTilePressed={ (): void => this.handleTilePressed(counter, symbol) }
-        />
-      );
-    }
-    else if (symbol === -1) {
-      return ( 
-        <Tile
-          key={ Math.random() } 
-          color={ this.props.color } 
-          blank 
-          margin={ this.props.margin }
-          handleTilePressed={ (): void => this.handleTilePressed(counter, symbol) }
-        />
-      );
-    }
-    else {
-      return ( 
-        <Tile 
-          key={ Math.random() } 
-          color={ this.props.color } 
-          disabled 
-          margin={ this.props.margin } 
-          handleTilePressed={ (): void => this.handleTilePressed(counter, symbol) }
-        /> 
-      );
-    }
   };
 
   render(): React.ReactElement {
